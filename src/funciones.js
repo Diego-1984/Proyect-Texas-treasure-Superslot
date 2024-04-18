3a7127fd51f1cbe49c16d789f781d303f2d230a4
Diego-1984 <diegomenoruceta@gmail.com>
2024-04-18 17:33:00 +0000

    Iniciado cambio de estilo de botton

--- src/funciones.js
+++ src/funciones.js
@@ -3,9 +3,11 @@ let combination = ["🍇", "🍉", "🍓", "🍎"];
 function getRandomNumber(arr) {
   return Math.floor(Math.random() * arr.length);
 }
+
 function getRandomItem(array) {
-  return array[getRandomNumber];
+  return array[getRandomNumber(array)]; // Corregido para pasar 'array' como argumento
 }
+
 function getSlot() {
   let slot = getRandomItem(combination);
   return slot;
